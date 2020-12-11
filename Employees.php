862686b91a3e177a9b805045db2547fa0952972b
Nathan De La Garza <nathanhale.delagarza@ndsu.edu>
2020-12-11 04:31:29 -0600

    To transfer to linux

---
+++ Employees.php
@@ -0,0 +1,26 @@
+<?php 
+include 'functions.php';  //Includes the php functions from the file functions.php in this file 
+// print_r(GetData()); //Calls the GetData function to return the requested data 
+?> 
+
+<!DOCTYPE html>
+<!-- This is be the html page where we will display the data that we retrieve via php -->
+<html>
+<head>
+<link rel="stylesheet" href="styles.css" type="text/css">
+</head>
+<body>
+<div class="navigation">
+  <a class="active" href="#">Employees</a>
+  <a href="Hubs.php">Hubs</a>
+  <a href="Routes.php">Routes</a>
+</div>
+
+
+</body>
+
+
+
+
+
+</html>
